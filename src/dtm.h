c7e93d5df3d8977051415498cb936769415755f0
EinMilchBoss <christian.benezuela@gmail.com>
2025-03-22 18:56:37 +0100

    Read file line by line instead of string.

--- src/dtm.h
+++ src/dtm.h
@@ -1,31 +1,32 @@
 #ifndef DTM_H
 #define DTM_H
 
+#include <stdlib.h>
 #include <stdint.h>
+
+#define TRANSITIONS_MAX 128
 
 const extern int8_t MOVE_LEFT;
 const extern int8_t MOVE_NEUTRAL;
 const extern int8_t MOVE_RIGHT;
 
-typedef uint8_t state_t;
-
 struct transition {
-    state_t src_state;
-    state_t dst_state;
+    uint8_t src_state;
+    uint8_t dst_state;
     char src_symbol;
     char dst_symbol;
     int8_t move;
 };
 
 struct dtm {
-    state_t states_amount;
-    state_t start_state;
-    state_t accept_state;
-    state_t reject_state;
+    uint8_t states_amount;
+    uint8_t start_state;
+    uint8_t accept_state;
+    uint8_t reject_state;
 
     char *alphabet;
 
-    struct transition *transitions;
+    struct transition transitions[TRANSITIONS_MAX];
 };
 
 uint8_t string_to_uint8(const char *string);
--- src/main.c
+++ src/main.c
@@ -1,11 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
 #include <string.h>
 
 #include "dtm.h"
 
-int parse_state(state_t *state, const char *line, const char *tag, const size_t tag_length) {
+const char *const states_tag = "states";
+const size_t states_tag_length = strlen(states_tag);
+
+const char *const start_tag = "start";
+const size_t start_tag_length = strlen(start_tag);
+
+const char *const accept_tag = "accept";
+const size_t accept_tag_length = strlen(accept_tag);
+
+const char *const reject_tag = "reject";
+const size_t reject_tag_length = strlen(reject_tag);
+
+const char *const alphabet_tag = "alphabet";
+const size_t alphabet_tag_length = strlen(alphabet_tag);
+
+int parse_state(uint8_t *state, const char *line, const char *tag, const size_t tag_length) {
     // + 1 for the ':' at the end.
     const char *start_value = line + tag_length + 1;
     *state = string_to_uint8(start_value);
@@ -19,7 +33,7 @@ int parse_state(state_t *state, const char *line, const char *tag, const size_t 
     return EXIT_SUCCESS;
 }
 
-int parse_states_amount(state_t *state, const char *line, const char *tag, const size_t tag_length) {
+int parse_states_amount(uint8_t *state, const char *line, const char *tag, const size_t tag_length) {
     if (parse_state(state, line, tag, tag_length))
         return EXIT_FAILURE;
 
@@ -36,46 +50,32 @@ char* parse_alphabet(const char *line) {
     return (char*)line;
 }
 
-char *string_slice_to_string(const char *string_slice, const size_t string_slice_length) {
-    char *string = calloc(string_slice_length + 1, sizeof(*string));
-    strncpy(string, string_slice, string_slice_length);
-    return string;
-}
-
 int main(void) {
-    const char *const states_tag = "states";
-    const size_t states_tag_length = strlen(states_tag);
-
-    const char *const start_tag = "start";
-    const size_t start_tag_length = strlen(start_tag);
-
-    const char *const accept_tag = "accept";
-    const size_t accept_tag_length = strlen(accept_tag);
-
-    const char *const reject_tag = "reject";
-    const size_t reject_tag_length = strlen(reject_tag);
-
-    const char *const alphabet_tag = "alphabet";
-    const size_t alphabet_tag_length = strlen(alphabet_tag);
-
-    char content[] =
-        "states:5\n"
-        "start:0\n"
-        "accept:3\n"
-        "be for real"
-        "reject:4\n"
-        "alphabet:01"
-        "0,0=0,1,>";
-
     struct dtm dtm = {0};
+    size_t transition_index = 0;
 
-    char *strtok_save = NULL;
-    const char *line = strtok_r(content, "\n", &strtok_save);
-    for (size_t line_number = 1; line; line_number++) {
+    FILE *file = fopen("config.tm", "rb");
+    if (!file)
+        return EXIT_FAILURE;
+
+    char *line = NULL;
+    size_t line_capacity = 0;
+    ssize_t line_length = 0;
+    size_t line_number = 1;
+    while ((line_length = getline(&line, &line_capacity, file)) != -1) {
+        // Replace '\n' with '\0' because the parsing expects C strings (and also for better logging).
+        if (!feof(file))
+            line[line_length - 1] = 0;
+        printf("Reading line %zu: \"%s\"\n", line_number, line);
+
         // Ignore empty lines.
-        if (strlen(line) == 0)
+        if (!*line) {
+            printf("Skipped empty line %zu.\n", line_number);
             continue;
+        }
 
+        // TODO: Have a uint8_t for flags and flip them to 1 when already parsed.
+        // TODO: Then include the check of those bits before the `strncmp()` call to avoid unnecessary calls.
         if (strncmp(line, states_tag, states_tag_length) == 0) {
             if (parse_states_amount(&dtm.states_amount, line, states_tag, states_tag_length))
                 return EXIT_FAILURE;
@@ -91,12 +91,19 @@ int main(void) {
         } else if (strncmp(line, alphabet_tag, alphabet_tag_length) == 0) {
             // + 1 for the ':' at the end.
             dtm.alphabet = (char*)line + alphabet_tag_length + 1;
+            printf("Parsed 'alphabet' successfully.\n");
         } else {
-            // if (parse_transition(dtm.transitions))
-            //     return EXIT_FAILURE;
+            if (transition_index >= TRANSITIONS_MAX)
+                return EXIT_FAILURE;
+
+            if (parse_transition(&dtm.transitions[transition_index], line))
+                return EXIT_FAILURE;
+
+            printf("Parsed transition %zu.\n", transition_index + 1);
+            transition_index++;
         }
 
-        line = strtok_r(NULL, "\n", &strtok_save);
+        line_number++;
     }
 
     return EXIT_SUCCESS;
